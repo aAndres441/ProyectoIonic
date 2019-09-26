0696f26ce0ebec0e475e118306df21afb248cb59
VeraArias <andrarias@gmail.com>
2019-09-26 16:10:47 -0300

    aaa

--- src/app/pages/extra-expenses/components/index.ts
+++ src/app/pages/extra-expenses/components/index.ts
@@ -1,3 +1,2 @@
 export * from './extra-expenses-form/extra-expenses-form.component'
 export * from './extra-expenses-list/extra-expenses-list.component';
-export * from './extra-print/extra-print.component';
